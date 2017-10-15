93382772d13c1fc4fe584b5816273fb71cdbaa46
Naman Pujari <namanpujari913@gmail.com>
2017-10-15 09:12:34 -0400

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-mysqli_connect("localhost", "anjix_andrew", "SayedSalah7421!");
-
-if (mysqli_connect_error()) echo "0";
-else echo "1";
-
-mysqli_result::fetch_all([$resulttype = MYSQLI_NUM]);
-?>
